2692519c18ddd468c995bbb2acd9e7f3f34eca61
diksha-shrivastava13 <E21CSEU0293@bennett.edu.in>
2023-12-06 15:02:15 +0530

    Refactoring

---
+++ encoding.py
@@ -0,0 +1,42 @@
+from typing import List
+from pydantic import BaseModel
+import bentoml
+
+
+class InputSchema(BaseModel):
+    year: int
+    month: int
+    category: str
+    type: str
+
+
+class OutputSchema(BaseModel):
+    prediction: float
+
+
+def encode_categories(category: str, lst: list[int]) -> list[int]:
+    category_mapping = {
+        "alcohol accidents": [1, 0, 0],
+        "escape accidents": [0, 1, 0],
+        "traffic accidents": [0, 0, 1],
+    }
+
+    category_values = category_mapping.get(category.lower(), [0])
+    lst.extend(category_values)
+
+    return lst
+
+
+def encode_types(type_: str, lst: List[int]) -> List[int]:
+    type_mapping = {
+        "injured and killed": [1, 0, 0],
+        "total": [0, 1, 0],
+        "with personal injury": [0, 0, 1],
+    }
+    type_values = type_mapping.get(type_.lower(), [0])
+    lst.extend(type_values)
+
+    del lst[2]
+    del lst[2]
+    return lst
+
