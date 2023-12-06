6d8a43712af2df45fb4a618412e54aa18b2898cb
diksha-shrivastava13 <E21CSEU0293@bennett.edu.in>
2023-12-06 21:13:04 +0530

    Refactored

--- Frontend/model_handler.py
+++ Frontend/model_handler.py
@@ -3,7 +3,7 @@ from xgboost import XGBRegressor as xgb
 from encoding import encode_categories, encode_types
 import pandas as pd
 model = xgb()
-model.load_model("/Users/DELL/PycharmProjects/Traffic-Accidents-Model/models/model_file_name.json")
+model.load_model("models/model_file_name.json")
 bento_model = bentoml.xgboost.save_model("my_xgboost_model", model)
 loaded_model = bentoml.xgboost.load_model("my_xgboost_model")
 
